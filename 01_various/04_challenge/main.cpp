2959fa96e8242a6a1c6b6596b19744d6b5ba4910
takayuki.asoma <takayuki.asoma@woven-planet.global>
2024-04-15 13:57:02 +0900

    01_04

--- 01_various/04_challenge/main.cpp
+++ 01_various/04_challenge/main.cpp
@@ -1,1 +1,10 @@
 // コードを入力してください
+#include "unique_ptr.hpp"
+#include <iostream>
+
+int main(){
+    unique_int_ptr my_int(3);
+    std::cout << *my_int << std::endl;
+    unique_ptr<double> my_double(4.5);
+    std::cout << *my_double << std::endl;
+}
--- 01_various/04_challenge/unique_ptr.hpp
+++ 01_various/04_challenge/unique_ptr.hpp
@@ -1,1 +1,55 @@
 // コードを入力してください
+#pragma once
+#include <iostream>
+
+class unique_int_ptr{
+public:
+    unique_int_ptr(const int in){
+        ptr_ = new int{in};
+    };
+    ~unique_int_ptr(){
+        delete ptr_;
+    };
+    int *get(){
+        return ptr_;
+    };
+    int* release(){
+        auto temp = ptr_;
+        ptr_ = nullptr;
+        return temp;
+    };
+    int &operator*()
+    {
+        return *ptr_;
+    };
+
+private:
+    int *ptr_;
+};
+
+
+template <typename T>
+class unique_ptr{
+public:
+    unique_ptr(const T in){
+        ptr_ = new T{in};
+    };
+    ~unique_ptr(){
+        delete ptr_;
+    };
+    T *get(){
+        return ptr_;
+    };
+    T* release(){
+        auto temp = ptr_;
+        ptr_ = nullptr;
+        return temp;
+    };
+    T &operator*()
+    {
+        return *ptr_;
+    };
+
+private:
+    T *ptr_;
+};
